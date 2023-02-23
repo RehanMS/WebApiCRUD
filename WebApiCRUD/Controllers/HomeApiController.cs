57cf5dbd17054de0c862ad62667fd502bee19f96
mdraj <mdraj@DELL>
2023-02-23 12:09:55 +0530

    Add project files.

---
+++ WebApiCRUD/Controllers/HomeApiController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using WebApiCRUD.Models;
+
+namespace WebApiCRUD.Controllers
+{
+    public class HomeApiController : ApiController
+    {
+        RehanDBEntities1 db= new RehanDBEntities1();
+        [HttpGet]
+        public IHttpActionResult GetEmployees()
+        {
+            List<Employee> list = db.Employees.ToList();
+            return Ok(list);
+        }
+        [HttpGet]
+        public IHttpActionResult DetailsApi( int id)
+        {
+            var emp = db.Employees.Where(model => model.EmpId== id).FirstOrDefault();
+            return Ok(emp);
+        }
+        [HttpPost]
+        public IHttpActionResult Insert( Employee e)
+        {
+            db.Employees.Add(e);
+            db.SaveChanges();
+            return Ok();
+        }
+        [HttpPut]
+        public IHttpActionResult Update(Employee e)
+        {
+            db.Entry(e).State = EntityState.Modified;
+            db.SaveChangesAsync();
+           //var emp = db.Employees.Where(model=>model.EmpId==e.EmpId).FirstOrDefault();
+           // if(emp!=null)
+           // {
+           //     emp.EmpId = e.EmpId;
+           //     emp.EmpName = e.EmpName;
+           //     emp.Salary= e.Salary;
+           //     emp.Job = e.Job;   
+           //     emp.Dname = e.Dname;
+           //     db.SaveChangesAsync();
+           // }
+           // else
+           // {
+           //     return NotFound();
+           // }
+            return Ok();
+        }
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var emp = db.Employees.Where(model => model.EmpId == id).FirstOrDefault();
+            db.Entry(emp).State = EntityState.Deleted;
+            db.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ WebApiCRUD/Controllers/HomeMVCController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Web.Mvc;
+using WebApiCRUD.Models;
+
+namespace WebApiCRUD.Controllers
+{
+    public class HomeMVCController : Controller
+    {
+        HttpClient client = new HttpClient();
+        public ActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>();
+            client.BaseAddress = new Uri("http://localhost:50531/api/HomeApi");
+            var response = client.GetAsync("HomeApi");
+            response.Wait();
+
+            var  test = response.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Employee>>();
+                display.Wait();
+                employees = display.Result;
+            }
+            return View(employees);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            client.BaseAddress = new Uri("http://localhost:50531/api/HomeApi");
+            var response = client.PostAsJsonAsync<Employee>("HomeApi", emp);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+        [HttpGet]
+        public ActionResult Details( int id)
+        {
+            Employee emp = null;
+            client.BaseAddress = new Uri("http://localhost:50531/api/HomeApi");
+            var response = client.GetAsync("HomeApi?id=" +id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                emp = display.Result;
+            }
+            return View(emp);
+        }
+        public ActionResult Edit(int id)
+        {
+            Employee emp = null;
+            client.BaseAddress = new Uri("http://localhost:50531/api/HomeApi");
+            var response = client.GetAsync("HomeApi?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                emp = display.Result;
+            }
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+            client.BaseAddress = new Uri("http://localhost:50531/api/HomeApi");
+            var response = client.PutAsJsonAsync<Employee>("HomeApi",e);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+        }
+        public ActionResult Delete( int id)
+        {
+            Employee emp = null;
+            client.BaseAddress = new Uri("http://localhost:50531/api/HomeApi");
+            var response = client.GetAsync("HomeApi?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                emp = display.Result;
+            }
+            return View(emp);
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed( int id)
+        {
+            client.BaseAddress = new Uri("http://localhost:50531/api/HomeApi");
+            var response = client.DeleteAsync("HomeApi/"+id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Delete");
+        }
+    }
+}
